e15bca4eaaffd17b2160506f1fd1aafd1d8b691b
kdmukai <kdmukai@gmail.com>
2022-02-03 16:08:16 -0600

    mvp complete

--- slushpool/controller.py
+++ slushpool/controller.py
@@ -1,8 +1,10 @@
+import csv
 import logging
+
 from flask import Flask, Response, redirect, render_template, request, url_for, flash
+from flask import current_app as app
 from flask_login import login_required
 
-from cryptoadvance.specter.services.controller import user_secret_decrypted_required
 from .service import SlushpoolService
 
 
@@ -15,10 +17,55 @@ logger = logging.getLogger(__name__)
 slushpool_endpoint = SlushpoolService.blueprint
 
 
+
 @slushpool_endpoint.route("/")
 @login_required
-@user_secret_decrypted_required
 def index():
+    if app.specter.user_manager.get_user().has_service(SlushpoolService.id):
+        return redirect(url_for(f"{SlushpoolService.get_blueprint_name()}.payouts"))
+
     return render_template(
         "slushpool/index.jinja",
     )
+
+
+@slushpool_endpoint.route("/payouts", methods=["GET", "POST"])
+@login_required
+def payouts():
+    return render_template(
+        "slushpool/payouts.jinja",
+        service=SlushpoolService,
+        services=app.specter.service_manager.services,
+    )
+
+
+@slushpool_endpoint.route("/upload", methods=["GET", "POST"])
+@login_required
+def upload():
+    if request.method == "POST":
+        csv_file = request.files["csv_file"]
+        csv_data = csv_file.stream.read().decode("utf-8").splitlines()
+        SlushpoolService.import_payout_history_csv(csv_data)
+
+        return redirect(url_for(f"{SlushpoolService.get_blueprint_name()}.payouts"))
+
+    return render_template(
+        "slushpool/upload.jinja",
+    )
+
+
+
+@slushpool_endpoint.route("/settings", methods=["GET"])
+@login_required
+def settings():
+    return render_template(
+        "slushpool/settings.jinja",
+    )
+
+
+
+@slushpool_endpoint.route("/remove", methods=["POST"])
+@login_required
+def remove():
+    SlushpoolService.clear_slushpool_data()
+    return redirect("/")
--- slushpool/service.py
+++ slushpool/service.py
@@ -1,15 +1,71 @@
+import csv
+from cryptoadvance.specter.addresslist import Address
 from cryptoadvance.specter.services.service import Service, devstatus_beta
+from flask import current_app as app
+from flask_babel import lazy_gettext as _
 
 
-class Slushpool(Service):
+class SlushpoolService(Service):
     id = "slushpool"
-    name = "Slush Pool Importer"
+    name = "Slush Pool"
     icon = "slushpool/img/slushpool_icon.png"
     logo = "slushpool/img/slushpool_logo.png"
-    desc = "Sync your mining rewards to your Specter wallet"
+    desc = "Import your mining rewards history into Specter"
     has_blueprint = True
     blueprint_module = "slushpool.controller"
     devstatus = devstatus_beta
 
     # TODO: As more Services are integrated, we'll want more robust categorization and sorting logic
     sort_priority = 2
+
+
+    @classmethod
+    def default_address_label(cls):
+        # Have to str() it; can't pass a LazyString to json serializer
+        return str(_("Slush Pool payout"))
+
+
+    @classmethod
+    def import_payout_history_csv(cls, csv_data: str):
+        """
+            ['amount', 'user_fee', 'coin', 'address', 'date', 'transaction_id']
+            ['0.01018352', '0.00000000', 'BTC', 'bc1q*************', '2021-08-22 04:05:01', '********************']
+            ['0.01017610', '0.00000000', 'BTC', 'bc1q*************', '2021-09-12 04:05:06', '********************']
+        """
+        csv_reader = csv.reader(csv_data)
+        has_addr_match = False
+        for row in csv_reader:
+            print(row)
+            address = row[3]
+            for wallet_name, wallet in app.specter.user_manager.get_user().wallet_manager.wallets.items():
+                addr_obj: Address = wallet.get_address_obj(address)
+                if addr_obj:
+                    print(f"found {addr_obj.address} in {wallet_name}")
+                    has_addr_match = True
+                    if addr_obj["service_id"]:
+                        # Already associated with a service; leave it as-is
+                        pass
+                    else:
+                        SlushpoolService.reserve_address(
+                            wallet=wallet,
+                            address=addr_obj.address
+                        )
+                    break
+
+        if has_addr_match:
+            # Make sure Slush Pool shows up in the left nav
+            app.specter.user_manager.get_user().add_service(SlushpoolService.id)
+
+
+    @classmethod
+    def clear_slushpool_data(cls):
+        """
+            Remove all Slushpool Service associations from any associated address from
+            any wallet.
+        """
+        for wallet_name, wallet in app.specter.user_manager.get_user().wallet_manager.wallets.items():
+            for addr_obj in wallet.get_associated_addresses(service_id=SlushpoolService.id):
+                wallet.deassociate_address(addr_obj.address)
+        
+        user = app.specter.user_manager.get_user()
+        user.remove_service(SlushpoolService.id)
